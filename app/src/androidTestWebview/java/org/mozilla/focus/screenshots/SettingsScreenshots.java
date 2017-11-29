721208197157fd8cc23fc100affc9d10348c7d21
No-Jun Park <npark@mozilla.com>
2017-11-29 16:44:02 -0500

    update for autocomplete

--- app/src/androidTestWebview/java/org/mozilla/focus/screenshots/SettingsScreenshots.java
+++ app/src/androidTestWebview/java/org/mozilla/focus/screenshots/SettingsScreenshots.java
@@ -41,6 +41,7 @@ import static android.support.test.espresso.matcher.PreferenceMatchers.withTitle
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.containsString;
